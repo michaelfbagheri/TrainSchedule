144714b6a924ae2986271a46776bcfe1c82d45e4
mb480s <mike.f.bagheri@gmail.com>
2018-10-01 16:27:23 -0400

    Final version

--- assets/javaScript/trainApp.js
+++ assets/javaScript/trainApp.js
@@ -15,7 +15,7 @@ var database = firebase.database()
 
 $('#schedule-table').empty() 
 //Setup the table headers
-$('#schedule-table').append('<tr><td class="table-org">Train Name</td><td class="table-org">Train Destination</td><td class="table-org">Train Frequency</td><td class="table-org">Next Arrival</td><td class="table-org">Min TillNextTrain</td>')
+$('#schedule-table').append('<tr><td class="table-org output-title">Train Name</td><td class="table-org output-title">Train Destination</td><td class="table-org output-title">Frequency (min)</td><td class="table-org output-title">Next Arrival</td><td class="table-org output-title">Next Train (min)</td>')
 
 //setup the Connections Child Ref
 var connectionsRef = database.ref("/connections");
@@ -37,39 +37,15 @@ $('#submit-button').on('click', function(){
     var trainFreq = $('#train-freq').val().trim()
     var trainFirstArrival = $('#train-first-arrival').val().trim()
     $('.input-org').val('')
-    console.log(trainFirstArrival)
-    var firstTimeConverted = moment(trainFirstArrival,"HH:mm").subtract(1,"years")
-    console.log(firstTimeConverted)
-
-    var time = moment()
-    console.log('Current Time: ' + moment(time).format("hh:mm"))
     
-    var differenceInTime = moment().diff(moment(firstTimeConverted), "minutes")
-    console.log(differenceInTime)
-    
-    var tReminder = differenceInTime % trainFreq
-    console.log(tReminder)
-
-
-
-
-
-    // trainMinAway equation needs to be derived
-    var trainMinAway = trainFreq - tReminder
-    console.log(trainMinAway)
-    //Next Arrival needs to be calculate once the clock function is setup
-    var trainNextArrival = moment().add(trainMinAway, "minutes")
-    console.log("arrival Time: " + moment(trainNextArrival).format('hh:mm'))
-    trainNextArrival = moment(trainNextArrival).format('hh:mm')
+   
     var timestamp = firebase.database.ServerValue.TIMESTAMP
-    
     
     database.ref("/Trains").push({
         trainName,
         trainDestination,
+        trainFirstArrival,
         trainFreq,
-        trainMinAway,
-        trainNextArrival,
         timestamp
     }) 
 
@@ -78,10 +54,31 @@ $('#submit-button').on('click', function(){
 
 
 database.ref("/Trains").on('child_added',function(snapshot){
+
+    var tempTrainFreq = snapshot.val().trainFreq
+    console.log(tempTrainFreq)
+    var tempTrainFirstArrival = snapshot.val().trainFirstArrival
+    console.log(tempTrainFirstArrival)
+
+ 
+    var firstTimeConverted = moment(tempTrainFirstArrival,"HH:mm").subtract(1,"years")
+    console.log(firstTimeConverted)
+    var time = moment()
+    var differenceInTime = moment().diff(moment(firstTimeConverted), "minutes")
+    console.log(differenceInTime)
+    var tReminder = differenceInTime % tempTrainFreq
+    console.log(tReminder)
+    var trainMinAway = tempTrainFreq - tReminder
+    console.log(trainMinAway)
+    var trainNextArrival = moment().add(trainMinAway, "minutes")
+    trainNextArrival = moment(trainNextArrival).format('hh:mm')
+    console.log(trainNextArrival)
+
     if (snapshot.val() === null){
         return
     } else{
-        $('#schedule-table').append('<tr><td class="table-org">' + snapshot.val().trainName + '</td><td class="table-org">' +  snapshot.val().trainDestination + '</td><td class="table-org">' + snapshot.val().trainFreq +  '</td><td class="table-org">' + snapshot.val().trainNextArrival + '</td><td class="table-org">' + snapshot.val().trainMinAway + '</td>')
+    var trainMinAway = tempTrainFreq - tReminder
+    $('#schedule-table').append('<tr><td class="table-org">' + snapshot.val().trainName + '</td><td class="table-org">' +  snapshot.val().trainDestination + '</td><td class="table-org">' + snapshot.val().trainFreq +  '</td><td class="table-org">' + trainNextArrival + '</td><td class="table-org">' + trainMinAway + '</td>')
 
         
     }
